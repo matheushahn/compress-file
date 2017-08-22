5d18c57277aa76e1c8e2d547317eda1a02cd3bb5
Matheus Hahn <matheus.hahn@gmail.com>
2017-08-21 21:38:06 -0300

    fix promise chain and add more details

--- app2.js
+++ app2.js
@@ -1,79 +1,91 @@
+/*jshint esversion: 6 */  
+/*jslint devel: true */
 'use strict';
-console.time('App execution time')
+console.time('Run Time');
 
-const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'));
+const bluebird = require('bluebird');
+const fs = bluebird.promisifyAll(require('fs'));
 const compression = require('lossless-text-compression');
+let fileSizeDecompressed;
+let fileSizeCompressed;
 
-console.log('** Read File **')
-console.time('Read File')
+console.log('** Read File **');
+console.time('Read File');
 fs.readFileAsync('./alice29.txt', 'utf8')
   .then(compress)
   .then(writeCompressFile)
   .then(decompress)
   .then(writeDecompressFile)
-  .catch(err => console.log(err))
+  .then(data => {
+    console.log('\n\n** App Details **')
+    console.timeEnd('Run Time');
+    console.log(`Compression Rate = ${fileSizeDecompressed/fileSizeCompressed}`)
+  })
+  .catch(err => console.log(err));
 
-function dataToString (data, cb) {
+function dataToString (data) {
   return data.map(function(byte) {
-    return String.fromCharCode(byte)
-  }).join('')
+    return String.fromCharCode(byte);
+  }).join('');
 }
 
-function compress (data, cb) {
+function compress (data) {
   return new Promise(function (resolve, reject) {
-    console.timeEnd('Read File')
-    console.log(`File size before compress: ${data.length} bytes`)
+    console.timeEnd('Read File');
+    console.log(`File size before compress: ${data.length} bytes`);
 
     // compress file
-    console.log('\n\n** Compress File **')
-    console.time('Compress File')
+    console.log('\n\n** Compress File **');
+    console.time('Compress File');
     compression.encode(data)
       .then(compressed => {
-        console.timeEnd('Compress File')  
-        console.log(`File size after compress: ${compressed.length} bytes`)
-        resolve(compressed)
+        console.timeEnd('Compress File'); 
+        fileSizeCompressed = compressed.length;
+        console.log(`File size after compress: ${compressed.length} bytes`);
+        resolve(compressed);
       })
-      .catch(err => reject(err))
-  })
+      .catch(err => reject(err));
+  });
 }
 
-function writeCompressFile(data, cb) {
+function writeCompressFile(compressedData) {
   return new Promise(function (resolve, reject) {
-    console.time('Write Compressed File')
-    fs.writeFile('./temp/compressed.txt', data)
-      .then( compressed => {
-        console.log('\n\n** Write Compressed File **')
-        console.timeEnd('Write Compressed File')
-        resolve(compressed)
+    console.time('Write Compressed File');
+    fs.writeFileAsync('./temp/compressed.txt', compressedData)
+      .then(res => {
+        console.log('\n\n** Write Compressed File **');
+        console.timeEnd('Write Compressed File');
+        resolve(compressedData);
       })
-      .catch(err => reject(err))
-  })
+      .catch(err => reject(err));
+  });
 }
 
-function decompress(data, cb) {
+function decompress(fileContent) {
   return new Promise(function (resolve, reject) {
-    console.time('Decompress File')
-    console.log('\n\n** Decompress File **')
-    return compression.decode(fileContent)
+    console.time('Decompress File');
+    console.log('\n\n** Decompress File **');
+    compression.decode(fileContent)
       .then(decompressedFile => {
-        console.timeEnd('Decompress File')
-        let stringContent = dataToString(decompressedFile)
-        console.log(`File size after decompress: ${stringContent.length} bytes`)
-        resolve(stringContent)
+        fileSizeDecompressed = decompressedFile.length;
+        console.timeEnd('Decompress File');
+        console.log(`File size after decompress: ${decompressedFile.length} bytes`);
+        resolve(decompressedFile);
       })
-      .catch(err => reject(err))
-  })
+      .catch(err => reject(err));
+  });
 }
 
-function writeDecompressFile(data) {
-  console.time('Write Decompressed File')
-  fs.writeFile('./temp/decompressed.txt', data)
-    .then( response => {
-      console.log('\n\n** Write Decompressed File **')
-      console.timeEnd('Write Decompressed File')
-      console.timeEnd('App execution time')
-    })
-    .catch(err => console.log(err))
+function writeDecompressFile(decompressedData) {
+  return new Promise(function (resolve, reject) {
+    console.time('Write Decompressed File');
+    fs.writeFileAsync('./temp/decompressed.txt', decompressedData)
+      .then( response => {
+        console.log('\n\n** Write Decompressed File **');
+        console.timeEnd('Write Decompressed File');
+        resolve(response);
+      })
+      .catch(err => reject(err));
+    });
 }
 
