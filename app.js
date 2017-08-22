e599a4aa4387f51d8651a167c2d5cbd50dad56a3
Matheus Hahn <matheus.hahn@gmail.com>
2017-08-21 21:46:53 -0300

    version 3.0

--- app.js
+++ app.js
@@ -1,50 +1,84 @@
+/*jshint esversion: 6 */  
+/*jslint devel: true */
 'use strict';
+console.time('Run Time');
 
-const fs = require('fs');
-const deflate = require('deflate-js');
-let fileContent; 
+const bluebird = require('bluebird');
+const fs = bluebird.promisifyAll(require('fs'));
+const compression = bluebird.promisifyAll(require('deflate-js'));
+let fileSizeDecompressed;
+let fileSizeCompressed;
 
 console.log('** Read File **');
 console.time('Read File');
-fs.readFile('./alice29.txt', (err, data) => {
-  if (err) throw err;
-  console.timeEnd('Read File');
-  console.log(`File size before compress: ${data.length}`)
-  
-  // compress file
-  console.log('\n\n** Compress File **');
-  console.time('Compress File');
-  fileContent = deflate.deflate(data, 9);
-	console.timeEnd('Compress File');
-  console.log(`File size after compress: ${fileContent.length}`)
-	
-  // write compressed file
-  console.time('Write Compressed File');
-  fs.writeFile('./temp/compressed.txt', fileContent, (err) => {
-    if (err) throw err;
-    console.log('\n\n** Write Compressed File **');
-		console.timeEnd('Write Compressed File');
-  });
-
-  //decompress file
-  console.log('\n\n** Decompress File **');
-  console.time('Decompress File');
-  let decompressedFile = deflate.inflate(fileContent);
-  console.timeEnd('Decompress File');
-  let stringContent = dataToString(decompressedFile);
-  console.log(`File size after decompress: ${stringContent.length}`)
-
-  //write decompressed file
-  console.time('Write Decompressed File');
-  fs.writeFile('./temp/decompressed.txt', stringContent, (err) => {
-    if (err) throw err;
-    console.log('\n\n** Write Decompressed File **');
-    console.timeEnd('Write Decompressed File');
-  });
-});
+fs.readFileAsync('./alice29.txt')
+  .then(compress)
+  .then(writeCompressFile)
+  .then(decompress)
+  .then(writeDecompressFile)
+  .then(data => {
+    console.log('\n\n** App Details **')
+    console.timeEnd('Run Time');
+    console.log(`Compression Rate = ${fileSizeDecompressed/fileSizeCompressed}`)
+  })
+  .catch(err => console.log(err));
 
 function dataToString (data) {
   return data.map(function(byte) {
     return String.fromCharCode(byte);
   }).join('');
+}
+
+function compress (data) {
+  return new Promise(function (resolve, reject) {
+    console.timeEnd('Read File');
+    console.log(`File size before compress: ${data.length} bytes`);
+
+    // compress file
+    console.log('\n\n** Compress File **');
+    console.time('Compress File');
+    let compressed = compression.deflate(data, 9);
+    fileSizeCompressed = compressed.length;
+    console.timeEnd('Compress File'); 
+    console.log(`File size after compress: ${compressed.length} bytes`);
+    resolve(compressed);
+  });
+}
+
+function writeCompressFile(compressedData) {
+  return new Promise(function (resolve, reject) {
+    console.time('Write Compressed File');
+    fs.writeFileAsync('./temp/compressed.txt', compressedData)
+      .then(res => {
+        console.log('\n\n** Write Compressed File **');
+        console.timeEnd('Write Compressed File');
+        resolve(compressedData);
+      })
+      .catch(err => reject(err));
+  });
+}
+
+function decompress(fileContent) {
+  return new Promise(function (resolve, reject) {
+    console.time('Decompress File');
+    console.log('\n\n** Decompress File **');
+    let decompressedFile = compression.inflate(fileContent);
+    fileSizeDecompressed = decompressedFile.length;
+    console.timeEnd('Decompress File');
+    console.log(`File size after decompress: ${decompressedFile.length} bytes`);
+    resolve(decompressedFile);
+  });
+}
+
+function writeDecompressFile(decompressedData) {
+  return new Promise(function (resolve, reject) {
+    console.time('Write Decompressed File');
+    fs.writeFileAsync('./temp/decompressed.txt', decompressedData)
+      .then( response => {
+        console.log('\n\n** Write Decompressed File **');
+        console.timeEnd('Write Decompressed File');
+        resolve(response);
+      })
+      .catch(err => reject(err));
+    });
 }
